96d262250655523cc3867929ce9071e28e86953a
Lorenzo Fernandez <lorenzojofernandez@gmail.com>
2020-04-05 21:00:38 +1200

    adding social links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,8 +5,10 @@ const ContactPage = () => {
         <div>
             <h1>Contact Us</h1>
             <p>Call us on 123456789</p>
+            <button><a href="mailto:ashtontairua@gmail.com?Subject=Site%20Enquiry" target="_top">Contact Us Now</a></button>
         </div>
     )
 }
 
 export default ContactPage
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
 import { Link } from 'gatsby'
+import LinkedIn from '../../public/images/LinkedIn.png'
+import Facebook from '../../public/images/Facebook.png'
+
 const HomePage = () => {
     return (
-        <div> 
+        <header> 
             <h1>Asbestasus</h1>
             <h2>Welcome to our website!</h2>
             <ul>
@@ -10,8 +13,10 @@ const HomePage = () => {
                 <li><Link to="/about">What we do</Link> </li>
                 <li><Link to="/news">News</Link></li>
                 <li><Link to="/contact">Contact Us</Link></li>
+                <li><a href="https://www.linkedin.com/in/fernandezlorenzo/"><img src={LinkedIn}></img></a></li>
+                <li><a href="https://www.facebook.com"><img src={Facebook}></img></a></li>
             </ul>
-        </div>
+        </header>
     )
 }
 
