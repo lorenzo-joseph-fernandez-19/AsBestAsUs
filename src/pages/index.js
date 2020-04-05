a938b0fdc6215adc694c40b781521d3207aeac56
Lorenzo Fernandez <lorenzojofernandez@gmail.com>
2020-04-05 15:55:05 +1200

    linking pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from "react"
-
+import { Link } from 'gatsby'
 const HomePage = () => {
     return (
         <div> 
             <h1>Asbestasus</h1>
             <h2>Welcome to our website!</h2>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">What we do</Link> </li>
+                <li><Link to="/news">News</Link></li>
+                <li><Link to="/contact">Contact Us</Link></li>
+            </ul>
         </div>
     )
 }
