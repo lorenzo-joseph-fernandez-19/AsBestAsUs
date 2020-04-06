9b948312b3a2ec41b5feed012acaa9fcc5908b4c
Lorenzo Fernandez <lorenzojofernandez@gmail.com>
2020-04-07 07:06:02 +1200

    added wordpress plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,9 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    siteUrl: `https://www.example.com`,
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
@@ -19,5 +22,38 @@ module.exports = {
         icon: `src/images/icon.png`
       },
     },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "http://localhost:8888/AsBestAsUs",
+        protocol: "http",
+        hostingWPCOM: false,
+        // We will be using some advanced custom fields
+        useACF: true,
+        acfOptionPageIds: [],
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "http://localhost:8888/AsBestAsUs",
+          replacementUrl: "http://localhost:8888/AsBestAsUs",
+        },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        excludedRoutes: [],
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
+    `gatsby-plugin-sitemap`,
   ],
 }
