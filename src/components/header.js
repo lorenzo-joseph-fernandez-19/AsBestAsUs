44fbad063f4a10de2e70573b43ec4f28d6e56604
Lorenzo Fernandez <lorenzojofernandez@gmail.com>
2020-04-13 20:57:08 +1200

    updated Navbar with burger menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,56 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Logo from '../images/Logo.png'
+import logo from '../images/logo.png'
 
-const Header = () => {
+const NavbarItem = props => (
+  <Link className="navbar-item is-capitalized" to={props.page}>
+    {props.pagename}
+  </Link>
+)
+const NavbarBurger = props => (
+  <div
+    onClick={props.toggleMenu}
+    className={`navbar-burger burger ${props.active ? 'is-active' : ''}`}
+  >
+    <span />
+    <span />
+    <span />
+  </div>
+)
+
+export default class Header extends React.Component {
+  state = {
+    activeMenu: false,
+  }
+  toggleMenu = () => {
+    this.setState({
+      activeMenu: !this.state.activeMenu,
+    })
+  }
+  render() {
     return (
-        <header className="hero is-info is-large">
-            <div className="hero-head">
-                <nav className="navbar">
-                    <div className="container">
-                        <div className="navbar-brand">
-                            <a className="navbar-item"><Link to="/"><img src={Logo} alt="Logo"/></Link></a>
-                            <span className="navbar-burger burger" data-target="navbarMenuHeroA">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            </span>
-                        </div>
-                            <div id="navbarMenuHeroB" className="navbar-menu">
-                                <div className="navbar-end">
-                                    <a><Link className="navbar-item is-active" to="/">Home</Link></a>
-                                    <a><Link className="navbar-item is-active" to="/about">What we do</Link></a>
-                                    <a><Link className="navbar-item is-active" to="/news">News</Link></a>
-                                <span className="navbar-item">
-                                    <a className="button is-info is-inverted">
-                                <span className="icon">
-                                    <i className="fab fa-github"></i>
-                                </span>
-                                <span><Link to="/contact">Contact Us</Link></span>
-                                    </a>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </header>
+      <nav className="navbar is-fixed-top has-shadow is-transparent">
+        <div className="container">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <NavbarBurger
+            active={this.state.activeMenu}
+            toggleMenu={this.toggleMenu}
+          />
+        </div>
+        <div
+          className={`navbar-menu ${this.state.activeMenu ? 'is-active' : ''}`}
+        >
+          <div className="navbar-end">
+            <NavbarItem page="/" pagename="Home" />
+            <NavbarItem page="/about" pagename="What we do" />
+            <NavbarItem page="/contact" pagename="Contact Us"/>
+          </div>
+        </div>
+        </div>
+      </nav>
     )
+  }
 }
-export default Header
