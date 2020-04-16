b6c24d964153e1e6f2605e64142bf1a421dac0f8
Lorenzo Fernandez <lorenzojofernandez@gmail.com>
2020-04-16 12:57:40 +1200

    styling contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,62 +1,75 @@
 import React from 'react'
 import Layout from '../components/layout'
-import "../styles/mystyles.scss"
+import '../components/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const ContactPage = () => {
     return (
         <Layout>
-            <div className="section">
-            <h1 className="title has-text-centered">Contact Us</h1>
-            <form name="contact" className="box is-" method="post" netlify-honeypot="bot-field" data-netlify="true">
+            <section className="section is-medium has-background-white">
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-5">
+                        <div className="content has-text-black">
+                            <h1 className="title is-1">Get in Touch.</h1>
+                            <hr className="has-background-warning"></hr>
+                            <p><span>Feel free to send us a message with any of your queries and we'll get back to you as soon as we can.</span></p>
+                        </div>
+                    </div>
+                    <div className="column is-offset-1 is-6">
+            <form name="contact" className="box has-background-warning" method="post" netlify-honeypot="bot-field" data-netlify="true">
                 <input type="hidden" name="bot-field"/>
                 <input type="hidden" name="form-name" value="contact"/>
-                <div class="field is-primary">
-                    <label class="label">First Name</label>
-                    <div class="control">
-                        <input class="input is-warning" type="text" required/>
+                <div className="block">
+                    <label className="label">First Name *</label>
+                    <div className="block">
+                        <input className="input" type="text" required/>
+                    </div>
+                </div>
+
+                <div className="block">
+                    <label className="label">Last Name *</label>
+                    <div className="control">
+                        <input className="input" type="text" required/>
                     </div>
                 </div>
 
                 <div class="field">
-                    <label class="label">Last Name</label>
-                    <div class="control">
-                        <input class="input is-warning" type="text" required/>
-                    </div>
-                </div>
-
-                <div class="field">
-                    <label class="label">Phone Number</label>
-                    <div class="control">
-                        <input class="input is-warning" type="tel" required/>
-                    </div>
-                </div>
-
-                <div class="field">
-                    <label class="label">Email</label>
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input is-warning" type="email" required/>
-                        <span class="icon is-small is-left">
-                            <i class="fas fa-envelope"></i>
+                    <label className="label">Email *</label>
+                    <div className="control has-icons-left has-icons-right">
+                        <input className="input" type="email" required/>
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-envelope"></i>
                         </span>
                     </div>
                 </div>
 
-                <div class="field">
-                    <label class="label">Message</label>
-                    <div class="control">
-                        <textarea class="textarea is-warning" required></textarea>
+                <div className="field">
+                    <label className="label">Phone Number *</label>
+                    <div className="control">
+                        <input className="input" type="tel" required/>
                     </div>
                 </div>
 
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-warning">Submit</button>
+                <div className="field">
+                    <label className="label">Message</label>
+                    <div className="control">
+                        <textarea className="textarea" required></textarea>
+                    </div>
+                </div>
+
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-black">Send</button>
                     </div>
                 </div>
                 </form>
-                <p>Call us on </p> <a href="mailto:ashtontairua@gmail.com?Subject=Site%20Enquiry" target="_top">here</a>
+                {/* <p>Call us on </p> <a href="mailto:ashtontairua@gmail.com?Subject=Site%20Enquiry" target="_top">here</a> */}
+                </div>
+                </div>
             </div>
+            </section>
         </Layout>
     )
 }
